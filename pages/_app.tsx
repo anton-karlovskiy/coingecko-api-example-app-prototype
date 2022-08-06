7c33d637a1bba8855c59405c4ffef50d757f104f
anton-karlovskiy <antonkarlovskiy@outlook.com>
2022-08-07 01:27:46 +0300

    feat: add head content to MyApp

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,20 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import 'styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Exchanges</title>
+        <meta
+          name='viewport'
+          content='initial-scale=1.0, width=device-width' />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;

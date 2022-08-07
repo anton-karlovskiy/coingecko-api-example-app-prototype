56072d6162db4f7e6fc1b3855f014e0e0aca56e9
anton-karlovskiy <antonkarlovskiy@outlook.com>
2022-08-07 07:05:36 +0300

    feat: implement a side-effect (fetching exchanges) in Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,24 @@
 import type { NextPage } from 'next';
 
-const Home: NextPage = () => {
+import { CoingeckoExchangeResponse } from 'types/general.d';
+
+interface Props {
+  exchanges: Array<CoingeckoExchangeResponse>;
+}
+
+const Home: NextPage<Props> = ({ exchanges }) => {
+  console.log('exchanges => ', exchanges);
+
   return (
     <>Home</>
   );
 };
+
+export async function getServerSideProps() {
+  const res = await fetch('https://api.coingecko.com/api/v3/exchanges?per_page=10');
+  const data = await res.json();
+
+  return { props: { exchanges: data } };
+}
 
 export default Home;
---
+++ types/general.d.ts
@@ -0,0 +1,18 @@
+interface CoingeckoExchangeResponse {
+  country: string;
+  description: string;
+  has_trading_incentive: boolean;
+  id: string;
+  image: string;
+  name: string;
+  trade_volume_24h_btc: number;
+  trade_volume_24h_btc_normalized: number;
+  trust_score: number;
+  trust_score_rank: number;
+  url: string;
+  year_established: number;
+}
+
+export type {
+  CoingeckoExchangeResponse
+};
